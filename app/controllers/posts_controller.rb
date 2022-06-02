860d57572a9dd91fc48fd403f2cd6efaadad8bb1
Benjamin Block <bjblock@gmail.com>
2022-06-02 01:29:53 +0000

    ship it!

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,10 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all
+    respond_to do |format|
+      format.html
+      format.json { render :json => @posts }
+    end
   end
 
   def new
@@ -12,7 +16,8 @@ class PostsController < ApplicationController
     if @current_user
       @post = Post.new
       @post["body"] = params["post"]["body"]
-      @post["image"] = params["post"]["image"]
+      # @post["uploaded_image"] = params["post"]["uploaded_image"]
+      @post.uploaded_image.attach(params["post"]["uploaded_image"])
       @post["user_id"] = @current_user["id"]
       @post.save
     else
@@ -21,12 +26,12 @@ class PostsController < ApplicationController
     redirect_to "/posts"
   end
 
-  # before_action :allow_cors
-  # def allow_cors
-  #   response.headers['Access-Control-Allow-Origin'] = '*'
-  #   response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
-  #   response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
-  #   response.headers['Access-Control-Max-Age'] = '1728000'
-  # end
+  before_action :allow_cors
+  def allow_cors
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, PATCH, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token, Auth-Token, Email, X-User-Token, X-User-Email'
+    response.headers['Access-Control-Max-Age'] = '1728000'
+  end
 
 end
